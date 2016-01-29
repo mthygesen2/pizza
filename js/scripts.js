d4f4e9c1b27645daa0c1370416fd057125d5315b
Mao <maothygesen@gmail.com>
2016-01-29 10:08:20 -0800

    create basic functioning form for pizza order

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ function Pizza(pizzaSize, howMany, toppings) {
 Pizza.prototype.pizzaFullPrice = function() {
   var price = 5
   var numberToppings = this.toppings;
-  var toppingPrice = 1 * numberToppings.length;
+  var toppingPrice = 1 * numberToppings;
 
   if(this.pizzaSize === "medium") {
     var sizePrice = (price + 3);
@@ -20,6 +20,19 @@ Pizza.prototype.pizzaFullPrice = function() {
   return onePizzaPrice * this.howMany
 };
 
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+
+    var inputSize = $('input[name=size]:checked').val();
+    var inputToppings = $('input[name=toppings]:checked').length;
+    var inputHowMany = $('#userInput').val();
+    var newOrder = new Pizza (inputSize, inputHowMany, inputToppings);
+
+    $(".priceShow").text(newOrder.pizzaFullPrice());
+    $(".priceOutcome").show();
+  });
+});
 // Pizza.prototype.pizzaTopppingsPrice = function() {
 //   var numberToppings = this.toppings;
 //   var toppingPrice = 1 * numberToppings.length;
--- specs/specs.js
+++ specs/specs.js
@@ -1,13 +1,17 @@
 describe("Pizza", function() {
   it("will create the Pizza object with these specification", function() {
-    var testPizza = new Pizza("medium", 2, ["mushrooms", "olives"]);
+    var testPizza = new Pizza("medium", 2, 2);
     expect(testPizza.pizzaSize).to.equal("medium");
     expect(testPizza.howMany).to.equal(2);
-    expect(testPizza.toppings).to.eql(["mushrooms", "olives"])
+    expect(testPizza.toppings).to.equal(2)
   });
   it("will return the price of the full pizza order", function() {
-    var testPizza = new Pizza("medium", 2, ["mushrooms", "olives"]);
+    var testPizza = new Pizza("medium", 2, 2);
     expect(testPizza.pizzaFullPrice()).to.equal(20);
+  });
+  it("will return the price of the full pizza order", function() {
+    var testPizza = new Pizza("small", 1, 1);
+    expect(testPizza.pizzaFullPrice()).to.equal(6);
   });
   // it("will return the price of the toppings on the pizza", function(){
   //   var testPizza = new Pizza("medium", 2, ["mushrooms", "olives"]);
