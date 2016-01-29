77d4f0a25abee6967436b008f79c664f1734f8cc
Mao <maothygesen@gmail.com>
2016-01-29 08:21:09 -0800

    add specs for pizza size price

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,15 @@ function Pizza(pizzaSize, howMany, toppings) {
   this.howMany = howMany;
   this.toppings = toppings;
 }
+
+Pizza.prototype.pizzaSizePrice = function () {
+  var price = 5
+  if(this.pizzaSize === "medium") {
+    var sizePrice = (price + 3);
+  } else if(this.pizzaSize === "large") {
+    var sizePrice = (price + 5);
+  } else {
+    var sizePrice = price;
+  }
+  return sizePrice;
+};
--- specs/specs.js
+++ specs/specs.js
@@ -5,4 +5,8 @@ describe("Pizza", function() {
     expect(testPizza.howMany).to.equal(2);
     expect(testPizza.toppings).to.eql(["mushrooms", "olives"])
   });
+  it("will return the price of the pizza for the size", function() {
+    var testPizza = new Pizza("medium");
+    expect(testPizza.pizzaSizePrice()).to.equal(8);
+  });
 });
