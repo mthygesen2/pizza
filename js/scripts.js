7b68f901d75844a075e99f9177afc7ac42ac5c49
Mao <maothygesen@gmail.com>
2016-01-29 08:11:27 -0800

    add all specifications for Pizza object

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
 function Pizza(pizzaSize, howMany, toppings) {
   this.pizzaSize = pizzaSize;
+  this.howMany = howMany;
+  this.toppings = toppings;
 }
--- specs/specs.js
+++ specs/specs.js
@@ -1,6 +1,8 @@
 describe("Pizza", function() {
   it("will create the Pizza object with these specification", function() {
-    var testPizza = new Pizza("medium");
+    var testPizza = new Pizza("medium", 2, ["mushrooms", "olives"]);
     expect(testPizza.pizzaSize).to.equal("medium");
+    expect(testPizza.howMany).to.equal(2);
+    expect(testPizza.toppings).to.eql(["mushrooms", "olives"])
   });
 });
